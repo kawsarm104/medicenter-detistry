52e9435ba20c3a5dee4b9c591e05ecbcb2de8732
kawsarm104 <kawsarm104@gmail.com>
2021-10-18 15:14:52 +0600

    added navbar

--- src/Pages/Common/Header/Header.js
+++ src/Pages/Common/Header/Header.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../../../images/logo/medicenter-logo.png";
 
 const Header = () => {
-    return (
-        <div>
-            nav bar will be here 
-        </div>
-    );
+  return (
+    <Navbar bg="light" expand="lg" className="p-3">
+      <Container className="">
+        <Navbar.Brand as={NavLink} to="/">
+          <img
+            alt=""
+                      src={logo}
+                      
+            width="35"
+            height="35"
+            className="d-inline-block align-top"
+          />{" "}
+          React Bootstrap
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto ">
+            <Nav.Link as={NavLink} to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">
+              Contact
+            </Nav.Link>
+
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
