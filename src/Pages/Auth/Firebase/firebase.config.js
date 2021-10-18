09bc3977b6bc661c39fb69524417930dea881767
kawsarm104 <kawsarm104@gmail.com>
2021-10-18 10:25:51 +0600

    set up firebase

---
+++ src/Pages/Auth/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: "AIzaSyBPpi84pNErCYE6bqTVCelcYc_DmrY4hik",
+  authDomain: "medicenter-dentist.firebaseapp.com",
+  projectId: "medicenter-dentist",
+  storageBucket: "medicenter-dentist.appspot.com",
+  messagingSenderId: "689227992182",
+  appId: "1:689227992182:web:458060537058e951cb2357",
+};
+
+export default firebaseConfig;
